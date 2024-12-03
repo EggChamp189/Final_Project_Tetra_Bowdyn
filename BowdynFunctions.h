9ba05c22009bf586c6f2d6adf981c2af77d44ea9
Skugi_Graffiti <frootloop.addict@hotmail.com>
2024-12-03 16:46:29 -0500

    Caesar Cipher decryption (vitals)

--- BowdynFunctions.h
+++ BowdynFunctions.h
@@ -1,1 +1,10 @@
 #pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/////// FUNCTION PROTOTYPES /////////////////
+
+string decryptCaesar(string encryption, int shift);
+int RetrieveEncryption();
--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include "TetraFunctions.h"
+#include "BowdynFunctions.h"
 /* use this to test my code, while also changing the alphabet in my header file
-string text = "I'm literally going to go joe biden mode deluxe edition!!!!1901910";
+string text = "I'm literally going to go joe biden mode deluxe edition!!!!1901910"; wooooooooo tetra
 string encodedText = EncodeVigenere("KEY", text);
 string decodedText = DecodeVigenere("KEY", encodedText);
 cout << "The original text was: " << text << endl;
 cout << "The decoded version of " << encodedText << " is:\n" << decodedText << endl;*/
+
+int main()
+{
+	//lol do thingz hereeeee VVVVVVVV
+	RetrieveEncryption();
+}
