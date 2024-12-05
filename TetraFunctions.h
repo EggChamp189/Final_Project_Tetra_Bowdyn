298d7b74f2b8cf2375f9b7c8d71032de6c443000
eggchamp189 <tetra.gee@mymail.champlain.edu>
2024-12-05 00:10:01 -0500

    Functions update
    menu function in main

--- TetraFunctions.h
+++ TetraFunctions.h
@@ -6,12 +6,5 @@
 
 using namespace std;
 
-const int MAX_SIZE = 1024;
-const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-
-string EncodeVigenere(string key, string wordToEncode);
-string DecodeVigenere(string key, string wordToDecode);
 string RegularEncodeVigenere(string key, string wordToEncode);
 string RegularDecodeVigenere(string key, string wordToDecode);
-bool CheckAlphabet(char letter);
-int ReturnAlphabetNumber(char letter);
